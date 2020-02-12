e0caf00e70daa1a2466ed209e2ed5d952160236a
Pham Anh Dung <anhdungpham090@gmail.com>
2020-02-13 01:53:43 +0700

    add admin theme and create dashboard index view.

---
+++ GiveAidProject/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace GiveAidProject.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
